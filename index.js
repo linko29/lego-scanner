01429686bf9f81271fe58fa52fe365d765b7b898
linko29 <linko02910@gmail.com>
2026-05-17 17:45:40 +0200

    Update index.js

--- index.js
+++ index.js
@@ -57,6 +57,8 @@ app.post("/scan", async (req, res) => {
     }
 });
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log("API running");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+    console.log("API running on port " + PORT);
 });
